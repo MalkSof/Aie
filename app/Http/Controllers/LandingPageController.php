fede8e617a1e410396c4664ce3f0927a5b5ce728
MalkSof <42155335+MalkSof@users.noreply.github.com>
2018-10-16 15:57:10 -0300

    Fecha Bien

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -22,13 +22,15 @@ class LandingPageController extends Controller
         if (setting('site.horario')>= date("H:i")) {
               # code...
             session()->put('fecha',date('Y-m-d'));
+            $fecha = date('Y-m-d');
             
           }
           else{
             session()->put('fecha',date('Y-m-d', strtotime(date('Y-m-d'). ' + 1 days')));
+              $fecha = date('Y-m-d', strtotime(date('Y-m-d'). ' + 1 days');
           }  
         
-        $comida = Comida::inRandomOrder()->where( fecha('D') , '1')->get();
+        $comida = Comida::inRandomOrder()->where( $fecha('D') , '1')->get();
     
         
         return view('landing-page')->with('comidas', $comida);
