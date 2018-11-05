ee7bd7161f78d6c12a08381f1b0815e6318b6485
MalkSof <42155335+MalkSof@users.noreply.github.com>
2018-11-05 14:24:45 -0300

    Fecha Pedido

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -44,7 +44,7 @@ use Illuminate\Support\Facades\DB;
 
             <div class="container">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-
+<?php echo ("Pedido Para:" . session()->get('fecha'));?>
 
                 <h1 >Comidas</h1>
 
